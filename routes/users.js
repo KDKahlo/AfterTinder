79811cb032bf2cc9608fc15a801f06c39dd902e1
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-04-25 12:43:44 +0200

    get id endpoint for relationships table

--- routes/users.js
+++ routes/users.js
@@ -11,38 +11,51 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-//
+//Register new user and populate users table.
 router.post("/register", async (req, res) => {
   const { email, firstname, password } = req.body;
-
   try {
     const hash = await bcrypt.hash(password, saltRounds);
-
     await db(
       `INSERT INTO users (email, firstname, password) VALUES ("${email}", "${firstname}", "${hash}")`
     );
-
     res.send({ message: "Register successful" });
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
 });
 
+//register new relationship and populate relationships table
 router.post("/relationships", async (req, res) => {
   const { code } = req.body;
-
   try {
-
     await db(
       `INSERT INTO relationships (code) VALUES ("${code}")`
     );
-
+    const result = await db(`SELECT * FROM relationships WHERE code = "${code}" `)
+      console.log(result)
     res.send({ message: "Register successful" });
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
 });
 
+//Get id from relationships table.
+//Not sure we need this endpoint.
+//I created it to explore how we can get the id of a relationship so that we can send it to the users_relationships table.
+router.get("/relationships", async (req, res) => {
+  const { code } = req.body;
+  try {
+    const result = await db(`SELECT id FROM relationships WHERE code = "${code}" `)
+    console.log(result)
+  
+     res.send(result.data[0]);
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+//Populate users_relationships table to relate a specific user to a specific relationship.
 router.post("/users_relationships", async (req, res) => {
   const { user_id, relationship_id } = req.body;
 
