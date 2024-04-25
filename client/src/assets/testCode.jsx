31591b47e8fd09ee41b8a4d9b5bd84b6e33b28e7
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-04-25 11:15:31 +0200

    register new user backend endpoint

---
+++ client/src/assets/testCode.jsx
@@ -0,0 +1,9 @@
+//USER REGISTRATION CODE TO TEST IN POSTMAN
+//URL: http://localhost:4000/users/register
+//Body (make sure "raw" and "json" are selected):
+// {
+//     "email": "example2@example.com",
+//     "firstname": "Mary",
+//     "password": "password1234"
+//   }
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require("jsonwebtoken");
+var db = require("../model/helper");
+require("dotenv").config();
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+//
+router.post("/register", async (req, res) => {
+  const { email, firstname, password } = req.body;
+
+  try {
+    const hash = await bcrypt.hash(password, saltRounds);
+
+    await db(
+      `INSERT INTO users (email, firstname, password) VALUES ("${email}", "${firstname}", "${hash}")`
+    );
+
+    res.send({ message: "Register successful" });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
 });
 
 module.exports = router;
