05b2464d997c491f3030c2e760a11c3adddaa5d8
Colleen-Peden <colleenpeden15@yahoo.com>
2024-04-28 17:41:55 +0200

    Colleen´s first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,40 @@
-import './App.css'
+import './App.css';
+import Registration from "./components/Registration";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Quiz from "./components/Quiz";
+import ChatwithAI from "./components/ChatwithAI";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
- 
-
-  return "After Tinder says 'Hello World!'" 
+    return (
+        <>
+            <h1>After-Tinder</h1>
+            <ul>
+                <li>
+                    <Link to="/Registration">Registration</Link>
+                </li>
+                <li>
+                    <Link to="/Login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/Home">Home</Link>
+                </li>
+                <li>
+                    <Link to="/Quiz">Quiz</Link>
+                </li>
+                <li>
+                    <Link to="/ChatwithAI">Chat with AI</Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route path="/Registration" element={<Registration />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Home" element={<Home />} />
+                <Route path="/Quiz" element={<Quiz />} />
+                <Route path="/ChatwithAI" element={<ChatwithAI />} />
+            </Routes>
+        </>
+    );
 }
-
 export default App
