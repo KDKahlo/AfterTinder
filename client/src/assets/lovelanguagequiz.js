9e40a7d4e341d3a2d31f62018164d18ec64566c6
Colleen-Peden <colleenpeden15@yahoo.com>
2024-04-28 18:00:37 +0200

    second commit Colleen

---
+++ client/src/assets/lovelanguagequiz.js
@@ -0,0 +1,156 @@
+/ Credit: Pokemon data accessed from here: https://media.5lovelanguages.com/a3a05ec6-d8ff-4e09-92d8-82b9d204d8d3/PersonalProfileforSingles5LL.pdf
+module.exports =[
+{
+    "Instructions": "This quiz contains 30 paired statements. Please chose the statement that best defines what is most meaningful to you in your relationship. Both statements may (or may not) sound like they fit your situation but please choose the statement that captures the essence of what is most meaningful to you, the majority of the time. Allow 10 to 15 minutes to complete the profile. Take it when you are relaxed, and try not to rush through it.",
+    "Quizz": {
+      "Statement": "It's more meaningful to me when…",
+      "Options": [
+        {
+          "A": "I receive a loving note/text/email for no special reason from my loved one.",
+          "E": "My partner and I hug."
+        },
+        {
+          "B": "I can spend alone time with my partner -just the two of us.",
+          "D": "My partner does something practical to help me out."
+        },
+        {
+          "C": "My partner gives me a little gift as a token of our love for each other.",
+          "B": "I get to spend uninterrupted leisure time with my partner."
+        },
+        {
+          "D": "My partner unexpectedly does something for me like filling my car or doing the laundry.",
+          "E": "My partner and I touch."
+        },
+        {
+          "E": "My partner puts their arm around me when we're in public.",
+          "C": "My partner surprises me with a gift."
+        },
+        {
+          "B": "I'm around my partner, even if we're not really doing anything.",
+          "E": "I hold hands with my partner."
+        },
+        {
+          "C": "My partner gives me a gift.",
+          "A": "I hear \"I love you\" from my partner."
+        },
+        {
+          "E": "I sit close to my partner.",
+          "A": "I am complimented by my loved one for no apparent reason."
+        },
+        {
+          "B": "I get the chance to just \"hang out\" with my partner.",
+          "C": "I unexpectedly get small gifts from my partner."
+        },
+        {
+          "A": "I hear my partner tell me, \"I'm proud of you.\"",
+          "D": "My partner helps me with a task."
+        },
+        {
+          "B": "I get to do things with my partner.",
+          "A": "I hear supportive words from my partner."
+        },
+        {
+          "D": "My partner does things for me instead of just talking about doing nice things.",
+          "E": "I feel connected to my partner through a hug."
+        },
+        {
+          "A": "I hear praise from my partner.",
+          "C": "My partner gives me something that shows they were really thinking about me."
+        },
+        {
+          "B": "I'm able to just be around my partner.",
+          "E": "I get a back rub or massage from my partner."
+        },
+        {
+          "A": "My partner reacts positively to something I've accomplished.",
+          "D": "My partner does something for me that I know they don't particularly enjoy."
+        },
+        {
+          "E": "My partner and I kiss frequently.",
+          "B": "I sense my partner is showing interest in the things I care about."
+        },
+        {
+          "D": "My partner works on special projects with me that I have to complete.",
+          "C": "My partner gives me an exciting gift."
+        },
+        {
+          "A": "I'm complimented by my partner on my appearance.",
+          "B": "My partner takes the time to listen to me and really understand my feelings."
+        },
+        {
+          "E": "My partner and I share non-sexual touch in public.",
+          "D": "My partner offers to run errands for me."
+        },
+        {
+          "D": "My partner does a bit more than their normal share of the responsibilities we share (around the house, work-related, etc).",
+          "C": "I get a gift that I know my partner put thought into choosing."
+        },
+        {
+          "B": "My partner doesn't check their phone while we're talking.",
+          "D": "My partner goes out of their way to do something that relieves pressure on me."
+        },
+        {
+          "C": "I can look forward to a holiday because of a gift I anticipate receiving.",
+          "A": "I hear the words, \"I appreciate you\" from my partner."
+        },
+        {
+          "C": "My partner brings me a little gift after they have been traveling without me.",
+          "D": "My partner takes care of something I'm responsible to do but I feel too stressed to do at the time."
+        },
+        {
+          "B": "My partner doesn't interrupt me while I'm talking.",
+          "C": "Gift giving is an important part of our relationship."
+        },
+        {
+          "D": "My partner helps me out when they know I'm already tired.",
+          "B": "I get to go somewhere while spending time with my partner."
+        },
+        {
+          "E": "My partner and I are physically intimate.",
+          "C": "My partner gives me a little gift that they picked up in the course of their normal day."
+        },
+        {
+          "A": "My partner says something encouraging to me.",
+          "B": "I get to spend time in a shared activity or hobby with my partner."
+        },
+        {
+          "C": "My partner surprises me with a small token of their appreciation.",
+          "E": "My partner and I touch a lot during the normal course of the day."
+        },
+        {
+          "D": "My partner helps me out - especially if I know they're already busy.",
+          "A": "I hear my partner specifically tell me, \"I appreciate you.\""
+        },
+        {
+          "E": "My partner and I embrace after we've been apart for a while.",
+          "A": "I hear my partner say how much I mean to them."
+        }
+      ]
+    },
+    "Results": {
+      "A": "WORDS OF AFFIRMATION",
+      "B": "QUALITY TIME",
+      "C": "RECEIVING GIFTS",
+      "D": "ACTS OF SERVICE",
+      "E": "PHYSICAL TOUCH"
+    },
+    "Score explanation": "Which love language received the highest score? This is your primary love language. If point totals for two love languages are equal, you are 'bilingual' and have two primary love languages. And, if you have a secondary love language, or one that is close in score to your primary love language, this means that both expressions of love are important to you.",
+    "Interpretation": {
+      "partner-results": {
+        "Words of affirmation": "Words of affirmation is about showing and receiving love through spoken words, praise, or appreciation. When this is someone's primary love language, they enjoy kind words and encouragement, uplifting quotes, love notes, and cute text messages. You can make this person's day by complimenting them or pointing out what they do well.",
+        "Quality time": "Someone with this love language values your full presence when you are together. They feel most loved if you give them your undivided attention and spend time together in meaningful and interactive ways. This means putting down the cell phone, turning off the computer, making eye contact, thoughtfully interacting, and actively listening.",
+        "Receiving gifts": "For someone who resonates with this love language, gifts symbolize love, care, and affection. They treasure not only the gift itself but also the time and effort the gift-giver put into selecting it. People who enjoy receiving gifts as part of their primary love language do not necessarily expect large or expensive presents; it's more the effort and thoughtfulness behind the gift that count. When you take the time to pick out a gift specifically for them, it tells them you are thoughtful and really know and care about them and their preferences. People with this love language can often remember every little gift they have received from their loved ones because it makes such an impact on them.",
+        "Acts of service": "Acts of service are nice things you do for your partner that make them feel loved and appreciated, such as washing the dishes, running errands, vacuuming, putting gas in the car. If your partner's main love language is acts of service, they'll notice and appreciate little things you do for them. They tend to perform acts of service and kindness for others, too.",
+        "Physical touch": "A person with physical touch as their primary love language feels love through physical affection. Aside from sex, they feel loved when their partner holds their hand, touches their arm, or gives them a massage at the end of the day, for example. This person's idea of a wonderful date night might be cuddling on the couch while watching a movie, slow dancing together with a lot of physical contact, or taking a long walk together while holding hands. They feel most loved when physically interacting with their partner."
+      },
+      "oneself-results": {
+        "Words of Appreciation": "This love language involves expressing affection through verbal affirmation, such as compliments, gratitude, and encouragement. If this is your primary love language, you feel most loved when you receive sincere and positive words from your partner, which validate your worth and efforts.",
+        "Acts of Service": "This love language centers around demonstrating love through actions that serve or support your partner. Examples include helping with chores, running errands, or doing tasks to lighten their load. If this is your love language, you feel most loved when your partner takes initiative to help or assist you in practical ways.",
+        "Physical Touch": "Physical touch is about expressing love through physical contact, such as hugs, kisses, holding hands, or other forms of affectionate touch. If this is your primary love language, you feel most loved when you experience physical closeness and affection from your partner, which fosters emotional connection and intimacy.",
+        "Gifts": "This love language involves showing affection through thoughtful gifts or gestures. It's not about the monetary value but the sentiment behind the gift and the effort put into choosing it. If this is your love language, you feel most loved when you receive meaningful gifts that demonstrate thoughtfulness and consideration from your partner.",
+        "Quality Time": "Quality time is about spending meaningful, focused time together, engaging in activities or simply being present with each other. If this is your primary love language, you feel most loved when your partner prioritizes spending time with you, gives you their undivided attention, and actively participates in shared experiences that strengthen your bond."
+      }
+    },
+    "Disclaimer": "This profile is an excerpt from The 5 Love Languages (©2015. Northfield Publishing) Reproduction and distribution for use, personal and/or professional (workshops, organizations, churches, nonprofits, small groups, etc.), is permitted provided the profiles are distributed free of charge. You may not under any circumstances upload, share, transmit, or distribute this file digitally."
+  }
+  ]
