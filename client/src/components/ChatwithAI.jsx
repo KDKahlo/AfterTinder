dededfd1f13be88d40f7e4a7e717c8d8e498a57c
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-05-04 19:46:47 +0200

    add dropdown menu to get user's input for the query to the AI

--- client/src/components/ChatwithAI.jsx
+++ client/src/components/ChatwithAI.jsx
@@ -20,9 +20,9 @@ const [userLoveLanguages, setUserLoveLanguages] = useState(
 //store user's input to ask for recommendations.
 //this info should come from a drop down menu input.
 const [askRecommendationsInput, setAskRecommendationsInput] = useState ({
-  type: "trip",
-  occasion: "birthday",
-  primaryLoveLanguage: "recieve gifts",
+  type: "",
+  occasion: "",
+  primaryLoveLanguage: "",
   city: "no specific city"
 })
 
@@ -77,99 +77,101 @@ function refineText(rawAIText) {
     console.log(refinedText)
    }
 
-//    const type = [
-//     { label: 'Gifts', value: 'gifts' },
-//     { label: 'Plans', value: 'plans' },
-//     { label: 'Compliments', value: 'compliments' },
-//     { label: 'Physical touch gestures', value: 'non sexual intimacy gestures' },  
-//     { label: 'Act of service', value: 'act of service' },
-//     { label: 'Trips', value: 'trips' },  
-// ];
+//these variables store the labels and the values of the options in the drop down menu where user can select their choice.
+   const type = [
+    { label: 'Gifts', value: 'gifts' },
+    { label: 'Plans', value: 'plans' },
+    { label: 'Compliments', value: 'compliments' },
+    { label: 'Physical touch gestures', value: 'non sexual intimacy gestures' },  
+    { label: 'Act of service', value: 'act of service' },
+    { label: 'Trips', value: 'trips' },  
+];
 
-// const occasion = [
-//   { label: 'Birthday', value: 'birthday' },
-//   { label: 'Romantic date', value: 'romantic date' },
-//   { label: 'Normal day', value: 'normal day' },
-//   { label: 'Weekend trip', value: 'weekend trip' },  
-//   { label: 'Holidays', value: 'holidays' },
-//   { label: 'Anniversary', value: 'anniversary' },  
-// ];
+const occasion = [
+  { label: 'Birthday', value: 'birthday' },
+  { label: 'Romantic date', value: 'romantic date' },
+  { label: 'Normal day', value: 'normal day' },
+  { label: 'Weekend trip', value: 'weekend trip' },  
+  { label: 'Holidays', value: 'holidays' },
+  { label: 'Anniversary', value: 'anniversary' },  
+];
 
-// const primaryLoveLanguage = [
-//   { label: 'Physical touch', value: 'non sexual intimacy' },
-//   { label: 'Words of affirmation', value: 'Words of affirmation' },
-//   { label: 'Acts of service', value: 'Acts of service' },
-//   { label: 'Receive gifts', value: 'Receive gifts' },  
-//   { label: 'Quality time', value: 'Quality time' },  
-// ];
+const primaryLoveLanguage = [
+  { label: 'Physical touch', value: 'non sexual intimacy' },
+  { label: 'Words of affirmation', value: 'Words of affirmation' },
+  { label: 'Acts of service', value: 'Acts of service' },
+  { label: 'Receive gifts', value: 'Receive gifts' },  
+  { label: 'Quality time', value: 'Quality time' },  
+];
 
-// function handleSelectDropdown(event){
-//   const value = event.target.value;
-//   const name = event.target.name;
+function handleSelectDropdown(event){
+  const value = event.target.value;
+  const name = event.target.name;
 
-//   //setInput remembers previous state and it updates it with the new name and value. That means that when I write the lastname, it doesn't forget the firstname.
-//   setCompleteEntry(state => ({
-//     ...state,
-//     [name]: value
-//   }));
-//   // addEntry(dropdownEntry)
-// }
+  //setAskRecommendationsInput remembers previous state and it updates it with the new name and value. That means that when I select occasion, it doesn't forget the the type or the primary love language.
+  setAskRecommendationsInput(state => ({
+    ...state,
+    [name]: value
+  }));
+}
 
 
   return (
     <>
       <h1>Ask the AI</h1>
       <div className="card">
-      {/* <form 
-        className='input-form col col-12'
-        onSubmit={handleSubmit}>
+      <h5>Get recommendations based on user input</h5>
+      <form>
 
-        <label > Hours of sleep
+        <label > Type of recommendation
             <select 
                 className="input-box"
-                value={completeEntry.hours} 
+                value={askRecommendationsInput.type} 
                 onChange={handleSelectDropdown}
-                name = "hours">
-            {hours.map((hour,index) => (
+                name = "type">
+            {type.map((type,index) => (
                 <option 
-                    value={hour.value}
-                    key = {index}>{hour.label}</option>
+                    value={type.value}
+                    key = {index}>{type.label}</option>
             ))}
             </select>
         </label>
-        <label> Meditation
+        <label> Select occasion
             <select 
                 className="input-box"
-                value={completeEntry.meditation} 
-                name = "meditation"
+                value={askRecommendationsInput.occasion} 
+                name = "occasion"
                 onChange={handleSelectDropdown}>
-            {meditation.map((option, index) => (
+            {occasion.map((option, index) => (
                 <option 
                     value={option.value}
                     key = {index}>{option.label}</option>
             ))}
             </select>
         </label>
-        <label> Exercise
+        <label> Select primary love language
             <select 
                 className="input-box"
-                value={completeEntry.exercise} 
-                name = "exercise"
+                value={askRecommendationsInput.primaryLoveLanguage} 
+                name = "primaryLoveLanguage"
                 onChange={handleSelectDropdown}>
-            {exercise.map((option, index) => (
+            {primaryLoveLanguage.map((option, index) => (
                 <option value={option.value}
                 key = {index}>{option.label}</option>
             ))}
             </select>
-        </label> */}
+        </label>
+        </form>
+        <button onClick={(event)=>handleSubmitRecommendation("User input", event)} disabled={loading}>
+          {loading ? "Generating..." : "Generate Text"}
+        </button>
+
         <h5>Get recommendations based on love language</h5>
         <button onClick={(event)=>handleSubmitRecommendation("Love language", event)} disabled={loading}>
           {loading ? "Generating..." : "Generate Text"}
         </button>
-        <h5>Get recommendations based on user input</h5>
-        <button onClick={(event)=>handleSubmitRecommendation("User input", event)} disabled={loading}>
-          {loading ? "Generating..." : "Generate Text"}
-        </button>
+        
+
        
         {/* {generatedText !== null && <p>Generated Text: {generatedText}</p>} */}
         {generatedText && Array.isArray(generatedText) && generatedText.map((recommendation, index)=>  (<p key={index}>{recommendation}</p>))}
