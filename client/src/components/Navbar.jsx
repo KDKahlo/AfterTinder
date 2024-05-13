d472b3fb02f7fc08d3594101bb715cdb60b882c3
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-05-13 18:10:30 +0200

    navbar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import AuthContext from '../contexts/AuthContext';
+import After_Tinder_logo from "../assets/After_tinder_logo.png"
 
 function Navbar() {
     const { isLoggedIn, logout } = useContext(AuthContext);
@@ -12,44 +13,63 @@ function Navbar() {
     };
 
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            {isLoggedIn ? (
-                <>
-                    <Link to="/QuizInstructions">Quiz</Link>
-                    <Link to="/chatwithai">Romantic Ideas</Link>
-                    <Link to="/Profile">Profile</Link>
-                    <Link to="/relationships">Relationships</Link>
-                    <button onClick={handleLogout}>Logout</button>
-                </>
-            ) : (
-                <>
-                    <Link to="/login">Login</Link>
-                    <Link to="/registration">Register</Link>
-                </>
-            )}
+        <nav className="navbar navbar-expand-lg bg-white">
+            <div className="container">
+                <a className="navbar-brand" href="/">
+                <img src={After_Tinder_logo } alt = "brand"></img>
+                </a>
+                <button
+                    className="navbar-toggler shadow-none"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <i className='bx bx-menu'></i>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ms-auto">
+                        <Link 
+                            className="nav-link active" 
+                            to="/">Home</Link>
+                        {isLoggedIn ? (
+                            <>
+                                <Link 
+                                    className="nav-link active" aria-current="page"
+                                    to="/QuizInstructions">Quiz</Link>
+                                <Link 
+                                    className="nav-link active" 
+                                    to="/chatwithai">Romantic Ideas</Link>
+                                <Link 
+                                    className="nav-link active" 
+                                    to="/Profile">Profile</Link>
+                                <Link 
+                                    className="nav-link active" 
+                                    to="/relationships">Relationships</Link>
+                                <button 
+                                    className="btn btn-primary shadow-none"
+                                    onClick={handleLogout}>Logout</button>
+                            </>
+                        ) : (
+                            <>
+                                <Link 
+                                    className="nav-link active" 
+                                    to="/login">Login</Link>
+                                <Link 
+                                    className="nav-link active" 
+                                    to="/registration">Register</Link>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 }
 
-//         <nav>
-//             <Link to="/">Home</Link>
-//             {isLoggedIn ? (
-//                 <>
-//                     <Link to="/quiz">Quiz</Link>
-//                     <Link to="/chatwithai">Chat with AI</Link>
-//                     <Link to="/Profile">Profile</Link>
-//                     <Link to="/relationships">Relationships</Link>
-//                     <button onClick={handleLogout}>Logout</button>
-//                 </>
-//             ) : (
-//                 <>
-//                     <Link to="/login">Login</Link>
-//                     <Link to="/registration">Register</Link>
-//                 </>
-//             )}
-//         </nav>
-//     );
-// }
+
 
 export default Navbar;
