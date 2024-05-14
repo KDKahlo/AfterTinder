40d76c94717578039c29380cd57bfb0b9079c2b6
Cristina <65986306+ceaguilar93@users.noreply.github.com>
2024-05-14 23:35:59 +0200

    Home.jsx
    Styling to home, all is finished

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,103 +1,157 @@
 import React from "react";
-import LoveQuote from './LoveQuote';
+import LoveQuote from "./LoveQuote";
 import Hero from "./Hero";
+import Home_Container1 from "../assets/Home_Container1.png";
+import Home_Container2 from "../assets/Home_Container2.png";
+import Home_Container3 from "../assets/Home_Container3.png";
 
-import{Link} from "react-router-dom"
-import{Outlet} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function Home() {
-    return (
-        <div className="home-container">
-            {/* Hero Section */}
-            <section className="hero-section">
-                <Hero />
-            </section>
+  return (
+    <div className="home-container">
+      {/* Hero Section */}
+      <section className="hero-section">
+        <Hero />
+      </section>
 
-            {/* LoveQuote Section */}
-            <section className="love-quote-section mt-5 colored-section padded-section">
-                <div className="container mt-5">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <LoveQuote />
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            {/* Quizzes Section */}
-            <section className="quizzes-section mt-5 colored-section">
-                <div className="container">
-                    <div className="row">
-                        {/* Left Column */}
-                        <div className="col-md-6">
-                            <img src="src/assets/After_tinder_logo.png" alt="Quizzes Image" className="img-fluid" style={{ height: "100%" }} />
-                        </div>
-
-                        {/* Right Column */}
-                        <div className="col-md-6 d-flex align-items-center justify-content-center">
-                            <div className="card shadow-sm text-center w-100">
-                                <div className="card-body">
-                                    <h5 className="card-title">Quizzes</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">Start with our Love Languages Quiz to discover more about your relationship.</h6>
-                                    <Link to={"/QuizInstructions"}>
-                                        <button className="btn btn-primary">Start Quiz</button>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            {/* Relationships Section */}
-            <section className="relationships-section mt-5 colored-section">
-                <div className="container">
-                    <div className="row">
-                        {/* Left Column */}
-                        <div className="col-md-6 d-flex align-items-center justify-content-center">
-                            <div className="card shadow-sm text-center w-100">
-                                <div className="card-body">
-                                    <h5 className="card-title">Relationships</h5>
-                                    <p className="card-text">Manage your relationships and settings here.</p>
-                                    <Link to={"/relationships"}>
-                                        <button className="btn btn-primary">Manage Relationships</button>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-
-                        {/* Right Column */}
-                        <div className="col-md-6">
-                            <img src="src/assets/After_tinder_logo.png" alt="Relationships Image" className="img-fluid" style={{ height: "100%" }} />
-                        </div>
-                    </div>
-                </div>
-            </section>
-            
-            {/* Romantic Ideas Section */}
-            <section className="romantic-ideas-section mt-5 colored-section">
-                <div className="container">
-                    <div className="row">
-                        {/* Left Column */}
-                        <div className="col-md-6">
-                            <img src="src/assets/After_tinder_logo.png" alt="Romantic Ideas Image" className="img-fluid" style={{ height: "100%" }} />
-                        </div>
-
-                        {/* Right Column */}
-                        <div className="col-md-6 d-flex align-items-center justify-content-center">
-                            <div className="card shadow-sm text-center w-100">
-                                <div className="card-body">
-                                    <h5 className="card-title">Romantic Ideas</h5>
-                                    <p className="card-text">Need ideas? Start a chat with our AI to get personalized romantic ideas based on your love language.</p>
-                                    <Link to={"/chatwithai"}>
-                                        <button className="btn btn-primary">Chat with AI</button>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+      {/* LoveQuote Section */}
+      <section className="love-quote-section mt-5 colored-section padded-section">
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-md-12">
+              <LoveQuote />
+            </div>
+          </div>
         </div>
-    );
+      </section>
+
+      {/* Quizzes Section */}
+      <section
+        className="quizzes-section mt-5 colored-section"
+        style={{ backgroundColor: "white" }}
+      >
+        <div className="container">
+          <div className="row">
+            {/* Left Column */}
+            <div className="col-md-6">
+              <img
+                src={Home_Container1}
+                alt="Quizzes Image"
+                className="img-fluid"
+                style={{ height: "100%" }}
+              />
+            </div>
+
+            {/* Right Column */}
+            <div className="col-md-6 d-flex align-items-center justify-content-center">
+              <div className="text-card" style={{ textAlign: "left" }}>
+                <h5
+                  className="card-title"
+                  style={{ fontSize: "48px", paddingBottom: "10px" }}
+                >
+                  Quizzes
+                </h5>
+                <h6
+                  className="card-subtitle mb-2 text-muted"
+                  style={{ fontSize: "18px", paddingBottom: "30px" }}
+                >
+                  Get ready for an "aha" moment!
+                  Start with the Love Languages Quiz® to find out how you want to be loved.
+                </h6>
+                <Link to={"/QuizInstructions"}>
+                  <button className="btn btn-primary">Start Quiz</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Relationships Section */}
+      <section className="relationships-section mt-5 colored-section">
+        <div className="container">
+          <div className="row">
+            {/* Left Column */}
+            <div className="col-md-6 d-flex align-items-center justify-content-center">
+              <div className="text-card" style={{ textAlign: "left" }}>
+                <h5
+                  className="card-title"
+                  style={{ fontSize: "48px", paddingBottom: "10px" }}
+                >
+                  Relationships
+                </h5>
+                <p
+                  className="card-text"
+                  style={{ fontSize: "18px", paddingBottom: "30px" }}
+                >
+                  Relationships go two ways. Once you discover your love language, it's time to share.
+                  Add the person (or persons) you are dating to the app so you can love them better.
+                </p>
+                <Link to={"/relationships"}>
+                  <button className="btn btn-primary">
+                    Go to my Relationships
+                  </button>
+                </Link>
+              </div>
+            </div>
+
+            {/* Right Column */}
+            <div className="col-md-6">
+              <img
+                src={Home_Container2}
+                alt="Relationships Image"
+                className="img-fluid"
+                style={{ height: "100%" }}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Romantic Ideas Section */}
+      <section
+        className="romantic-ideas-section mt-5 colored-section"
+        style={{ backgroundColor: "white" }}
+      >
+        <div className="container">
+          <div className="row">
+            {/* Left Column */}
+            <div className="col-md-6">
+              <img
+                src={Home_Container3}
+                alt="Romantic Ideas Image"
+                className="img-fluid"
+                style={{ height: "100%" }}
+              />
+            </div>
+
+            {/* Right Column */}
+            <div className="col-md-6 d-flex align-items-center justify-content-center">
+              <div className="text-card" style={{ textAlign: "left" }}>
+                <h5
+                  className="card-title"
+                  style={{ fontSize: "48px", paddingBottom: "10px" }}
+                >
+                  Romantic Ideas
+                </h5>
+                <h6
+                  className="card-subtitle mb-2 text-muted"
+                  style={{ fontSize: "18px", paddingBottom: "30px" }}
+                >
+                  People grow closer when they choose to consistently speak
+                each other’s love language.
+                Need ideas? Ask our AI and get personalized suggestions for your loved ones.
+                </h6>
+                <Link to={"/chatwithai"}>
+                  <button className="btn btn-primary">Chat with AI</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,90 +1,122 @@
-import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import AuthContext from '../contexts/AuthContext';
-import After_tinder_logo_NAVBAR from "../assets/After_tinder_logo_NAVBAR.png"
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthContext from "../contexts/AuthContext";
+import After_tinder_logo_NAVBAR from "../assets/After_tinder_logo_NAVBAR.png";
 
 function Navbar() {
-    const { isLoggedIn, logout } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const { isLoggedIn, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        logout();
-        navigate("/login");  // Redirect to login page after logout
-    };
+  const handleLogout = () => {
+    logout();
+    navigate("/login"); // Redirect to login page after logout
+  };
 
-    return (
-        <nav className="navbar navbar-expand-lg" style={{backgroundColor: "#857ff0", paddingTop: '20px', paddingBottom: "10px" }}>
-            <div className="container">
-                <a className="navbar-brand" href="/">
-                <img src={After_tinder_logo_NAVBAR} alt = "brand"></img>
-                </a>
-                <button
-                    className="navbar-toggler shadow-none"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
+  return (
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{
+        backgroundColor: "#857ff0",
+        paddingTop: "20px",
+        paddingBottom: "10px",
+      }}
+    >
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img src={After_tinder_logo_NAVBAR} alt="brand"></img>
+        </a>
+        <button
+          className="navbar-toggler shadow-none"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="bx bx-menu"></i>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <Link
+              className="nav-link active"
+              to="/"
+              style={{ color: "white", transition: "color 0.3s" }}
+              onMouseEnter={(e) => (e.target.style.color = "black")}
+              onMouseLeave={(e) => (e.target.style.color = "white")}
+            >
+              Home
+            </Link>
+            {isLoggedIn ? (
+              <>
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/QuizInstructions"
+                  style={{ color: "white", transition: "color 0.3s" }}
+                  onMouseEnter={(e) => (e.target.style.color = "black")}
+                  onMouseLeave={(e) => (e.target.style.color = "white")}
                 >
-                    <i className='bx bx-menu'></i>
+                  Quiz
+                </Link>
+                <Link
+                  className="nav-link active"
+                  to="/chatwithai"
+                  style={{ color: "white", transition: "color 0.3s" }}
+                  onMouseEnter={(e) => (e.target.style.color = "black")}
+                  onMouseLeave={(e) => (e.target.style.color = "white")}
+                >
+                  Romantic Ideas
+                </Link>
+                <Link
+                  className="nav-link active"
+                  to="/Profile"
+                  style={{ color: "white", transition: "color 0.3s" }}
+                  onMouseEnter={(e) => (e.target.style.color = "black")}
+                  onMouseLeave={(e) => (e.target.style.color = "white")}
+                >
+                  Profile
+                </Link>
+                <Link
+                  className="nav-link active"
+                  to="/relationships"
+                  style={{ color: "white", transition: "color 0.3s" }}
+                  onMouseEnter={(e) => (e.target.style.color = "black")}
+                  onMouseLeave={(e) => (e.target.style.color = "white")}
+                >
+                  Relationships
+                </Link>
+                
+                <button
+                  className="btn custom-btn"
+                  onClick={handleLogout}
+                >
+                  Logout
                 </button>
-
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav ms-auto">
-                        <Link 
-                            className="nav-link active" 
-                            to="/">Home</Link>
-                        {isLoggedIn ? (
-                            <>
-                                <Link 
-                                    className="nav-link active" aria-current="page"
-                                    to="/QuizInstructions"
-                                    style={{ color: 'white', transition: 'color 0.3s' }} 
-                                    onMouseEnter={(e) => e.target.style.color = '#857ff0'}
-                                    onMouseLeave={(e) => e.target.style.color = 'black'}>Quiz</Link>
-                                <Link 
-                                    className="nav-link active" 
-                                    to="/chatwithai"
-                                    style={{ color: 'white', transition: 'color 0.3s' }} 
-                                    onMouseEnter={(e) => e.target.style.color = '#857ff0'}
-                                    onMouseLeave={(e) => e.target.style.color = 'black'}
-                                    >Romantic Ideas</Link>
-                                <Link 
-                                    className="nav-link active" 
-                                    to="/Profile"
-                                    style={{ color: 'white', transition: 'color 0.3s' }} 
-                                    onMouseEnter={(e) => e.target.style.color = '#857ff0'}
-                                    onMouseLeave={(e) => e.target.style.color = 'black'}
-                                    >Profile</Link>
-                                <Link 
-                                    className="nav-link active" 
-                                    to="/relationships"
-                                    style={{ color: 'white', transition: 'color 0.3s' }} 
-                                    onMouseEnter={(e) => e.target.style.color = '#857ff0'}
-                                    onMouseLeave={(e) => e.target.style.color = 'black'}
-                                    >Relationships</Link>
-                                <button 
-                                    className="btn btn-primary shadow-none"
-                                    onClick={handleLogout}>Logout</button>
-                            </>
-                        ) : (
-                            <>
-                                <Link 
-                                    className="nav-link active" 
-                                    to="/login">Login</Link>
-                                <Link 
-                                    className="nav-link active" 
-                                    to="/registration">Register</Link>
-                            </>
-                        )}
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+              </>
+            ) : (
+              <>
+                <Link className="nav-link active" to="/login"
+                style={{ color: "white", transition: "color 0.3s" }}
+                onMouseEnter={(e) => (e.target.style.color = "black")}
+                onMouseLeave={(e) => (e.target.style.color = "white")}
+                >
+                  Login
+                </Link>
+                <Link className="nav-link active" to="/registration"
+                style={{ color: "white", transition: "color 0.3s" }}
+                onMouseEnter={(e) => (e.target.style.color = "black")}
+                onMouseLeave={(e) => (e.target.style.color = "white")}>
+                  Register
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
-
-
 
 export default Navbar;
