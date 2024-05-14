32c13e83d66b2dbce4bbe355c72c2308aaf188e6
Cristina <65986306+ceaguilar93@users.noreply.github.com>
2024-05-14 20:09:19 +0200

    Navbar.jsx modified

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import AuthContext from '../contexts/AuthContext';
-import After_Tinder_logo from "../assets/After_tinder_logo.png"
+import After_tinder_logo_NAVBAR from "../assets/After_tinder_logo_NAVBAR.png"
 
 function Navbar() {
     const { isLoggedIn, logout } = useContext(AuthContext);
@@ -13,10 +13,10 @@ function Navbar() {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg bg-white">
+        <nav className="navbar navbar-expand-lg" style={{backgroundColor: "#857ff0", paddingTop: '20px', paddingBottom: "10px" }}>
             <div className="container">
                 <a className="navbar-brand" href="/">
-                <img src={After_Tinder_logo } alt = "brand"></img>
+                <img src={After_tinder_logo_NAVBAR} alt = "brand"></img>
                 </a>
                 <button
                     className="navbar-toggler shadow-none"
@@ -39,16 +39,31 @@ function Navbar() {
                             <>
                                 <Link 
                                     className="nav-link active" aria-current="page"
-                                    to="/QuizInstructions">Quiz</Link>
+                                    to="/QuizInstructions"
+                                    style={{ color: 'white', transition: 'color 0.3s' }} 
+                                    onMouseEnter={(e) => e.target.style.color = '#857ff0'}
+                                    onMouseLeave={(e) => e.target.style.color = 'black'}>Quiz</Link>
                                 <Link 
                                     className="nav-link active" 
-                                    to="/chatwithai">Romantic Ideas</Link>
+                                    to="/chatwithai"
+                                    style={{ color: 'white', transition: 'color 0.3s' }} 
+                                    onMouseEnter={(e) => e.target.style.color = '#857ff0'}
+                                    onMouseLeave={(e) => e.target.style.color = 'black'}
+                                    >Romantic Ideas</Link>
                                 <Link 
                                     className="nav-link active" 
-                                    to="/Profile">Profile</Link>
+                                    to="/Profile"
+                                    style={{ color: 'white', transition: 'color 0.3s' }} 
+                                    onMouseEnter={(e) => e.target.style.color = '#857ff0'}
+                                    onMouseLeave={(e) => e.target.style.color = 'black'}
+                                    >Profile</Link>
                                 <Link 
                                     className="nav-link active" 
-                                    to="/relationships">Relationships</Link>
+                                    to="/relationships"
+                                    style={{ color: 'white', transition: 'color 0.3s' }} 
+                                    onMouseEnter={(e) => e.target.style.color = '#857ff0'}
+                                    onMouseLeave={(e) => e.target.style.color = 'black'}
+                                    >Relationships</Link>
                                 <button 
                                     className="btn btn-primary shadow-none"
                                     onClick={handleLogout}>Logout</button>
