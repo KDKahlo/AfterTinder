8c8d8873ab91924bbfa2790402f327adfb357069
Cristina <65986306+ceaguilar93@users.noreply.github.com>
2024-05-15 15:17:17 +0200

    Update RelationshipsProfile.jsx
    Changes for delete partner frontend

--- client/src/components/RelationshipsProfile.jsx
+++ client/src/components/RelationshipsProfile.jsx
@@ -1,50 +1,47 @@
 import React, { useState, useEffect } from "react";
 import PartnersData from "./PartnersData";
-import ShowPartnersData from "./ShowPartnersData";
-import { Link, Outlet } from "react-router-dom";
+import { Link} from "react-router-dom";
 import HeroRelationships from "./HeroRelationships";
 
 export default function RelationshipsProfile() {
+    const [loadingPartners, setLoadingPartners] = useState(true)
+    const navigate = useNavigate()
   const [userLoveLanguages, setUserLoveLanguages] = useState([]);
 
   useEffect(() => {
-    setUserLoveLanguages([]);
-  }, []);
+    setLoadingPartners(false)
+    }, [userLoveLanguages]);
 
   function updateUserLoveLanguages(data) {
     setUserLoveLanguages(data);
   }
+
+  function handleDeleteClick() {
+    navigate("/relationships/delete")
+
+}
 
   return (
     <div className="home-container">
       {/* Adds HeroRelationships */}
       <HeroRelationships />
       
-      <PartnersData
-        updateUserLoveLanguages={(userData) =>
-          updateUserLoveLanguages(userData)
-        }
-      />
-
-      {userLoveLanguages && userLoveLanguages.length ? (
-        <>
-          <h4>Your partners:</h4>
-          {userLoveLanguages.map((partner, index) => (
-            <div key={index}>
-              <Link
-                to={{
+      <PartnersData updateUserLoveLanguages={(userData)=> updateUserLoveLanguages(userData)}/>
+    {loadingPartners ? <p>Loading partners...</p> :
+      <>
+      <h4>Your partners:</h4> 
+      {userLoveLanguages.map((partner, index)=>(
+          <div key= {index}>
+              <Link 
+              to={{
                   pathname: `/relationships/${index}`,
-                }}
-                state={partner}
-              >
-                {partner.firstname}
-              </Link>
-            </div>
-          ))}
-        </>
-      ) : (
-        <p>Loading partners...</p>
-      )}
-    </div>
+              }}
+              state= {partner}
+              >{partner.firstname}</Link>
+          </div>
+      ))}
+       </> }
+       </div>
   );
+
 }
