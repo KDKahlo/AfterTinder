6df56889205246e3b3c63022bacfb1aa9d36b881
Cristina <65986306+ceaguilar93@users.noreply.github.com>
2024-05-15 14:56:48 +0200

    HeroRomanticIdeas added and AIDropdownInput modified
    HeroRomanticIdeas was added to the component AIDropdownInput
    
    All components of the app now have hero sections

--- client/src/components/HeroRomanticIdeas.jsx
+++ client/src/components/HeroRomanticIdeas.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function HeroProfile() {
+export default function HeroRomanticIdeas() {
   return (
     <section className="hero">
       <div className="container">
@@ -8,17 +8,30 @@ export default function HeroProfile() {
           {/* Columna Izquierda */}
           <div className="col-md-6">
             <div className="copy">
-              <div className="text-label">My Profile</div>
-              <div className="text-hero-bold">Welcome</div>
+              <div className="text-label">Romantic Ideas</div>
+              <div className="text-hero-bold">(Real) love is in the air</div>
               <div className="text-hero-regular">
-                It's never too late to start loving better!
+              It's time to speak your partner's love language. Just select the 
+              options bellow and After-Tinder will give you great ideas!
+    
               </div>
             </div>
           </div>
-          {/* Columna Derecha - considerar poner una pic de branding here */}
+          {/* Columna Derecha */}
           <div className="col-md-6">
             <div className="copy">
-              
+              <div
+                style={{
+                  fontSize: "var(--header-4)",
+                  display: "grid",
+                  placeItems: "center",
+                  height: "100%",
+                  paddingTop: "120px" /* Agregar padding superior */,
+                  color: "white",
+                }}
+              > Show them you care!
+                
+              </div>
             </div>
           </div>
         </div>
